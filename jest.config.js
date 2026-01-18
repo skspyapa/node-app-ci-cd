8fd5d6658164839bc7356570b83aed21b48f7feb
skspyapa <skspyapa@gmail.com>
2026-01-18 20:11:52 +0530

    update jest file

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
-{
-  "testEnvironment": "node",
-  "coveragePathIgnorePatterns": ["/node_modules/"],
-  "testMatch": ["**/__tests__/**/*.test.js", "**/?(*.)+(spec|test).js"],
-  "collectCoverageFrom": ["src/**/*.js", "!src/index.js"],
-  "coverageThresholds": {
-    "global": {
-      "branches": 70,
-      "functions": 70,
-      "lines": 70,
-      "statements": 70
+module.exports = {
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).js'],
+  collectCoverageFrom: ['src/**/*.js', '!src/index.js'],
+  coverageThresholds: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
     }
   }
-}
+};
